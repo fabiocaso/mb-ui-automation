4f91c192a3c73a78800c68fb7955c0edac363441
Fabio <caso.fabio@gmail.com>
2026-01-16 15:53:11 +0100

    Add config-driven execution and GitHub Actions CI

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,18 +3,30 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.ConfigReader;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
+        String browser = ConfigReader.get("browser");
+        boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
+        String baseUrl = ConfigReader.get("base.url");
+
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        if (headless) {
+            options.addArguments("--headless=new");
+        }
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.malwarebytes.com");
+        driver.get(baseUrl);
     }
 
         @AfterMethod
