44dcf7d32ae8e3c40daef4a85ee8a83ebd5e02cc
Fabio <caso.fabio@gmail.com>
2026-01-18 10:14:49 +0100

    Updating Screenshots and Login tests

---
+++ src/test/java/listeners/TestListener.java
@@ -0,0 +1,8 @@
+package listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    // your listener methods
+}
--- src/test/java/tests/UtilsTests/StringUtilsTest.java
+++ src/test/java/tests/UtilsTests/StringUtilsTest.java
@@ -1,4 +1,4 @@
-package tests;
+package tests.UtilsTests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
